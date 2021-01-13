5d9d3c4e08aaeb24edf7f57c1d9d6ca08aaf0240
001GitHubUser <77382870+001GitHubUser@users.noreply.github.com>
2021-01-13 14:42:55 +0530

    Gammer
    Player -2

---
+++ gammer.py
@@ -0,0 +1,10 @@
+def gammer():
+	"""_THIS CHOICE FOR USER DEPEND ONLY_"""
+	list2=[ "snack","water","gun"]
+#	l=[" snacks "," water ", "gun "]
+	
+	print("\nLIST-->",list2)
+	ch=input("  ENTER A CHOICE :: ")
+	return ch
+	
+	
