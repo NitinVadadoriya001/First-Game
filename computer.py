c67ade0359cb04660e653b7a81a53e131e470a52
001GitHubUser <77382870+001GitHubUser@users.noreply.github.com>
2021-01-13 14:42:03 +0530

    Computer
    Comutet player-2

---
+++ computer.py
@@ -0,0 +1,8 @@
+def computer():
+	"""_THIS CHOICE IS RANDOMLY SELECT BY COMPUTER[ RANDOM MODULES ]"""
+	import random
+	list1=[ "snack" , "water" , "gun"]
+	#l2=[ "s","w","g"]
+	return random.choice(list1)
+	
+
